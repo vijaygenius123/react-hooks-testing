7692d2a5240b410b7b485c0e2257b1f73e7c1950
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-12 23:04:55 +0530

    Basic Testing Setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Basic Test', ()=> {
+  it('works', function () {
+      expect(2 + 2).toEqual(4);
+  });
+})
